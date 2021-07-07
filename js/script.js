767b17129269beeda87b027ea4d22724bf26049e
skvoretc <skvoretc00@gmail.com>
2021-07-07 13:57:01 +0300

    final

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $('.list').slick({
         infinite: false,
         slidesToShow: 8,
-        slidesToScroll: 7,
+        slidesToScroll: 1,
         appendArrows: $('.dots'),
         responsive: [{
             breakpoint: 700,
@@ -22,42 +22,36 @@ $(document).ready(function() {
             breakpoint: 940,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1,
             }
         },
         {
             breakpoint: 1200,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 2,
             }
         },
         {
             breakpoint: 1700,
             settings: {
                 slidesToShow: 4,
-                slidesToScroll: 3,
             }
         },
         {
             breakpoint: 2300,
             settings: {
                 slidesToShow: 5,
-                slidesToScroll: 4,
             }
         },
         {
             breakpoint: 2800,
             settings: {
                 slidesToShow: 6,
-                slidesToScroll: 5,
             }
         },
         {
             breakpoint: 3200,
             settings: {
                 slidesToShow: 7,
-                slidesToScroll: 6,
             }
         }]
     });
